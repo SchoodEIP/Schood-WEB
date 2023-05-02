fb0d08533f1a765859953315cc6128d0577c3bab
adbusnel <adrien.busnel@epitech.eu>
2023-05-03 00:47:37 +0200

    [MOD] added graph space and question space for School Admin homePage

--- web/src/Users/SchoolAdmin/SchoolAdmHomePage.jsx
+++ web/src/Users/SchoolAdmin/SchoolAdmHomePage.jsx
@@ -3,6 +3,8 @@ import { LastAlerts } from "../../Components/Alerts/LastAlerts";
 import HeaderComp from "../../Components/Header/HeaderComp";
 import Sidebar from '../../Components/AdminMenu/index';
 import "./SchoolAdmHomePage.css";
+import { QuestSpace } from "../../Components/Questionnaire/QuestSpace";
+import { GraphSpace } from "../../Components/Graph/GraphSpace";
 
 export default function SchoolAdmHomePage() {
     return (
@@ -13,6 +15,10 @@ export default function SchoolAdmHomePage() {
             <div className="page-body">
                 <div className="left-half">
                     <Sidebar />
+                </div>
+                <div className="middle-half">
+                    <GraphSpace />
+                    <QuestSpace />
                 </div>
                 <div className="right-half">
                     <LastAlerts />
