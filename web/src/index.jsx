65bb7bced6711f0bfecd4eeddb15c31f0f4b4cec
adbusnel <adrien.busnel@epitech.eu>
2023-05-03 00:15:49 +0200

    Revert "[ADD] Déconnection, Landing Page, HomePage for Admin, HomePage for School Admin, Accounts Page for School Admins"
    This reverts commit 7ddcdfafe47f93add88902bed038e24c299fa8cf.

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './styles/sidebar.scss';
-import AdmHomePage from './Users/Admin/AdmHomePage';
-import AdmAccountsPage from './Users/Admin/AdmAccountsPage';
-import SchoolAdmHomePage from './Users/SchoolAdmin/SchoolAdmHomePage';
-import SchoolAdmAccountsPage from './Users/SchoolAdmin/SchoolAdmAccountsPage';
-import LandingPage from './Users/Public/LandingPage';
+import Sidebar from './pages/Sidebar';
 
 const rootElement = document.getElementById('root');
 
@@ -15,11 +11,7 @@ if (rootElement) {
   root.render(
     <Router>
       <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/Adm/Home" component={AdmHomePage} />
-        <Route exact path="/Adm/Accounts" component={AdmAccountsPage} />
-        <Route exact path="/School/Home" component={SchoolAdmHomePage} />
-        <Route exact path="/School/Accounts" component={SchoolAdmAccountsPage} />
+        <Route exact path="/" component={Sidebar} />
       </Switch>
     </Router>
   );
