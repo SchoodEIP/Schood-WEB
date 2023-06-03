4491f3747aa1888a4301bd5e67d1e9aa8d5443a0
adbusnel <adrien.busnel@epitech.eu>
2023-06-03 13:24:57 +0200

    [FIX] correct syntax error no page test, [ADD] added landing page test

--- src/tests/NoPage.test.js
+++ src/tests/NoPage.test.js
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import NoPage from '../Users/Public/NoPage'
 
-describe('HeaderComp', () => {
-  test('should render logo and user icon', () => {
+describe('NoPage', () => {
+  test('should render error message', () => {
     render(<NoPage />)
     const error = screen.getByText('Error 404')
     const message = screen.getByText('This page does not exist.')
