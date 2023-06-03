82263f7222612659ca84b9b7f9bcec0edb6a15b9
Leo-Nord <eleonore.wichegrod@epitech.eu>
2023-06-03 16:57:33 +0200

    [FIX] Dashboard Student tests

--- src/tests/dashboardStudent.test.jsx
+++ src/tests/dashboardStudent.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, act } from '@testing-library/react';
 import axios from 'axios';
-import Dashboard from './';
+import Dashboard from '../Users/Student/dashboardStudent';
 
 jest.mock('axios');
 
