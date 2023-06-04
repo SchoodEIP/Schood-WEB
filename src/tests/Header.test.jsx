896ab8fe8e97bba9999e19164f1f41acb336cb46
adbusnel <adbusnel@users.noreply.github.com>
2023-06-04 10:48:21 +0000

    [LINT] Applied linter fix

--- src/tests/Header.test.jsx
+++ src/tests/Header.test.jsx
@@ -1,10 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import HeaderComp from '../Components/Header/HeaderComp'
-import { fireEvent } from '@testing-library/react'
 
 describe('HeaderComp', () => {
-
   test('should render logo and user icon', () => {
     render(<HeaderComp />)
     const logo = screen.getByAltText('logo')
@@ -18,10 +16,10 @@ describe('HeaderComp', () => {
     const logout = screen.getByTestId('logout-button')
     expect(logout.tagName).toBe('A')
 
-    window.localStorage.setItem('token', 'falseToken');
-    window.localStorage.setItem('role', 'admin');
-    window.sessionStorage.setItem('token', 'falseToken');
-    window.sessionStorage.setItem('role', 'admin');
+    window.localStorage.setItem('token', 'falseToken')
+    window.localStorage.setItem('role', 'admin')
+    window.sessionStorage.setItem('token', 'falseToken')
+    window.sessionStorage.setItem('role', 'admin')
     expect(window.localStorage.getItem('token')).toBe('falseToken')
     expect(window.localStorage.getItem('role')).toBe('admin')
     expect(window.sessionStorage.getItem('token')).toBe('falseToken')
@@ -33,7 +31,5 @@ describe('HeaderComp', () => {
     expect(window.localStorage.getItem('role')).toBe(null)
     expect(window.sessionStorage.getItem('token')).toBe(null)
     expect(window.sessionStorage.getItem('role')).toBe(null)
-
   })
-
 })
