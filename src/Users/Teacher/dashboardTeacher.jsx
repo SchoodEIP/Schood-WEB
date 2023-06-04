4986324e1b11c038eb80bf67df29a96079e0d1ee
adbusnel <adrien.busnel@epitech.eu>
2023-06-04 11:48:35 +0200

    [FIX] removed linter errors and modified based on merge comments

--- src/Users/Teacher/dashboardTeacher.jsx
+++ src/Users/Teacher/dashboardTeacher.jsx
@@ -5,31 +5,31 @@ import { QuestSpace } from '../../Components/Questionnaire/QuestSpace'
 import { GraphSpace } from '../../Components/Graph/GraphSpace'
 import { LastAlerts } from '../../Components/Alerts/LastAlerts'
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 
 const TeacherHomePage = () => {
-  const [moodData, setMoodData] = useState([])
-  const [questionnairesData, setQuestionnairesData] = useState([])
-  const [alertsData, setAlertsData] = useState([])
+  // const [moodData, setMoodData] = useState([])
+  // const [questionnairesData, setQuestionnairesData] = useState([])
+  // const [alertsData, setAlertsData] = useState([])
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+  // useEffect(() => {
+  //   fetchData()
+  // }, [])
 
-  const fetchData = async () => {
-    try {
-      const responseMood = await axios.get('http://schood.fr/mood')
-      setMoodData(responseMood.data)
+  // const fetchData = async () => {
+  //   try {
+  //     const responseMood = await axios.get('http://schood.fr/mood')
+  //     setMoodData(responseMood.data)
 
-      const responseQuestionnaires = await axios.get('http://schood.fr/questionnaires')
-      setQuestionnairesData(responseQuestionnaires.data)
+  //     const responseQuestionnaires = await axios.get('http://schood.fr/questionnaires')
+  //     setQuestionnairesData(responseQuestionnaires.data)
 
-      const responseAlerts = await axios.get('http://schood.fr/alerts')
-      setAlertsData(responseAlerts.data)
-    } catch (error) {
-      console.error(error)
-    }
-  }
+  //     const responseAlerts = await axios.get('http://schood.fr/alerts')
+  //     setAlertsData(responseAlerts.data)
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
 
   return (
     <div className='dashboard'>
--- src/tests/Sidebar.test.jsx
+++ src/tests/Sidebar.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { act } from 'react-dom/test-utils'
+import { act, MouseEvent } from 'react-dom/test-utils'
 import Sidebar from '../Components/Sidebar/Sidebar'
 
 let container = null
