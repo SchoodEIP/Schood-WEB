32858342090ca3c54bace4ba9fbef23044d748b2
adbusnel <adrien.busnel@epitech.eu>
2023-08-31 15:55:02 +0200

    [MOD] removed facility addition

--- src/Users/Admin/admAccountsPage.js
+++ src/Users/Admin/admAccountsPage.js
@@ -9,20 +9,14 @@ import Popup from '../../Components/Popup/popup'
 export default function AdmAccountsPage () {
   const [isOpenSingle, setIsOpenSingle] = useState(false)
   const [isOpenMany, setIsOpenMany] = useState(false)
-  const [isOpenFacility, setIsOpenFacility] = useState(false)
   const [email, setEmail] = useState('')
   const [firstname, setFirstName] = useState('')
   const [lastname, setLastName] = useState('')
   const [fileName, setFile] = useState('')
-  const [name, setName] = useState('')
-  const [address, setAddress] = useState('')
-  const [telephone, setTelephone] = useState('')
-  const [level, setLevel] = useState(0)
   const [rolesList, setRolesList] = useState([])
   const [errMessage, setErrMessage] = useState('')
   const singleCreationUrl = process.env.REACT_APP_BACKEND_URL + '/adm/register'
   const csvCreationUrl = process.env.REACT_APP_BACKEND_URL + '/adm/csvRegisterUser'
-  const facilityUrl = process.env.REACT_APP_BACKEND_URL + '/admin/facility/register'
 
   useEffect(() => {
     const rolesUrl = process.env.REACT_APP_BACKEND_URL + '/adm/rolesList'
@@ -42,20 +36,6 @@ export default function AdmAccountsPage () {
     }
   }, [])
 
-  const toggleFacility = () => {
-    setIsOpenFacility(!isOpenFacility)
-    setName('')
-    setAddress('')
-    setTelephone('')
-    setLevel(0)
-    if (isOpenMany) {
-      setIsOpenMany(false)
-    }
-    if (isOpenSingle) {
-      setIsOpenSingle(false)
-    }
-  }
-
   const toggleSingleAccount = () => {
     setIsOpenSingle(!isOpenSingle)
     setFirstName('')
@@ -65,9 +45,6 @@ export default function AdmAccountsPage () {
     if (isOpenMany) {
       setIsOpenMany(false)
     }
-    if (isOpenFacility) {
-      setIsOpenFacility(false)
-    }
   }
 
   const toggleManyAccounts = () => {
@@ -76,9 +53,6 @@ export default function AdmAccountsPage () {
     setErrMessage('')
     if (isOpenSingle) {
       setIsOpenSingle(false)
-    }
-    if (isOpenFacility) {
-      setIsOpenFacility(false)
     }
   }
 
@@ -96,22 +70,6 @@ export default function AdmAccountsPage () {
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0])
-  }
-
-  const handleNameChange = (event) => {
-    setName(event.target.value)
-  }
-
-  const handleAddressChange = (event) => {
-    setAddress(event.target.value)
-  }
-
-  const handleTelephoneChange = (event) => {
-    setTelephone((event.target.value))
-  }
-
-  const handleLevelChange = (event) => {
-    setLevel(parseInt(event.target.value))
   }
 
   const singleAccountCreation = async (event) => {
@@ -154,7 +112,7 @@ export default function AdmAccountsPage () {
     event.preventDefault()
     const formData = new FormData()
 
-    formData.append('file', fileName)
+    formData.append('csv', fileName)
 
     try {
       await fetch(csvCreationUrl, {
@@ -165,39 +123,7 @@ export default function AdmAccountsPage () {
         body: formData
       }).then(response => {
         if (response.ok) {
-          window.location.reload()
-        } else {
-          const data = response.json()
-
-          setErrMessage(data.message)
-        }
-      })
-    } catch (e) {
-      setErrMessage(e.message)
-    }
-  }
-
-  const facilityRegister = async (event) => {
-    event.preventDefault()
-
-    const payload = {
-      name,
-      address,
-      telephone,
-      level
-    }
-
-    try {
-      await fetch(facilityUrl, {
-        method: 'POST',
-        headers: {
-          'x-auth-token': sessionStorage.getItem('token'),
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-      }).then(response => {
-        if (response.ok) {
-          window.location.reload()
+          setErrMessage("this is fine")
         } else {
           const data = response.json()
 
@@ -219,19 +145,6 @@ export default function AdmAccountsPage () {
           <Sidebar />
         </div>
         <div className='table-div'>
-          <div className='establishment-banner'>
-            <span className='banner-text'>Établissement : SchoodTmp</span>
-            <div className='banner-btn'>
-              <button
-                className='account-pop-up-btn'
-                data-testid='facility-account-btn'
-                onClick={toggleFacility}
-                style={{ backgroundColor: isOpenFacility ? '#8c52ff' : '#4f23e2' }}
-              >
-                Modifier les informations
-              </button>
-            </div>
-          </div>
           <AdmAccountsTable />
         </div>
         <div className='account-div'>
@@ -243,31 +156,6 @@ export default function AdmAccountsPage () {
           />
         </div>
       </div>
-      {
-        isOpenFacility && <Popup
-          handleClose={toggleFacility}
-          title={"Informations de l'établissement"}
-          errMessage={errMessage}
-          handleCreation={facilityRegister}
-          btn_text='Modifier'
-          content={
-            <div>
-              <form className='pop-form'>
-                <input className='pop-input' placeholder='Nom' onChange={handleNameChange} />
-                <input className='pop-input' placeholder='Addresse' onChange={handleAddressChange} />
-                <input className='pop-input' placeholder='Telephone' onChange={handleTelephoneChange} />
-                <select className='pop-input' placeholder='Level' onChange={handleLevelChange}>
-                  <option value='0'>Primaire</option>
-                  <option value='1'>Collège</option>
-                  <option value='2'>Lycée</option>
-                  <option value='3'>Supérieur</option>
-                  <option value='4'>Autre</option>
-                </select>
-              </form>
-            </div>
-          }
-                          />
-      }
       {
         isOpenSingle && <Popup
           handleClose={toggleSingleAccount}
