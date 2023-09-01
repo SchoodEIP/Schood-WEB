557834d75a35e23b95685bb3599de6de3ec67a53
adbusnel <adrien.busnel@epitech.eu>
2023-09-01 19:36:26 +0200

    [ADD] tests for formListPage

---
+++ src/__tests__/Users/Teacher/formListPage.test.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, act, screen, fireEvent } from '@testing-library/react'
+import FormListPage from '../../../Users/Teacher/formListPage'
+import { BrowserRouter } from 'react-router-dom'
+
+describe('FormListPage', () => {
+    test('page successfully created', async () => {
+        await act(async () => {
+            render(
+                <BrowserRouter>
+                  <FormListPage />
+                </BrowserRouter>
+              )
+        })
+        expect(screen.getByText('Mes Questionnaires')).toBeInTheDocument()
+        expect(screen.getByText('Créer un Nouveau Questionnaire +')).toBeInTheDocument()
+    })
+
+    test('button to create new forms works', async () => {
+        await act(async () => {
+            render(
+                <BrowserRouter>
+                  <FormListPage />
+                </BrowserRouter>
+              )
+        })
+
+        const originalLocation = window.location;
+
+        delete window.location;
+        window.location = {
+          href: '',
+        };
+
+        const newFormBtn = screen.getByText('Créer un Nouveau Questionnaire +')
+
+        await act(async () => {
+          fireEvent.click(newFormBtn)
+        })
+
+        expect(window.location.href).toBe('/questionnaire');
+
+        window.location = originalLocation;
+
+    })
+})
