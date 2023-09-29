13aa4539ee527cc17ff6c9411cdd23b565ac9208
adbusnel <adrien.busnel@epitech.eu>
2023-09-29 16:29:16 +0200

    [MOD] tests cypress

--- cypress/e2e/schood/admin.cy.jsx
+++ cypress/e2e/schood/admin.cy.jsx
@@ -1,24 +1,27 @@
 /// <reference types="cypress" />
 
-describe('Login Page', () => {
-    beforeEach(() => {
+describe('Admin', () => {
+    before(() => {
         cy.visit('http://localhost:3000/login')
     })
 
-    afterEach(() => {
+    after(() => {
         cy.clearAllLocalStorage()
         cy.clearAllSessionStorage()
     })
 
-    it('checks login works', () => {
+    it('checks admin part works', () => {
         cy.get('#userInput').type('admin@schood.fr')
         cy.get('#passInput').type('admin123')
-        cy.get('#login-button').click()
+        cy.get('#submit-button').click()
         cy.should(() => {
             expect(localStorage.getItem('role')).to.eq('admin')
             expect(sessionStorage.getItem('role')).to.eq('admin')
             expect(localStorage.getItem('token')).to.not.eq(null)
             expect(sessionStorage.getItem('token')).to.not.eq(null)
         })
+        cy.url().should('eq', 'http://localhost:3000/');
+        cy.get('#sidebar-item-1').click()
+        cy.url().should('eq', 'http://localhost:3000/accounts');
     })
 })
