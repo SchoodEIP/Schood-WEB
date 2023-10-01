e0e60eae880bc48d4ddacd6f95b98aa3592dc017
adbusnel <adrien.busnel@epitech.eu>
2023-10-01 17:16:13 +0200

    [MOD] changed teacher tests, school admin tests

--- cypress/e2e/schood/schoolAdmin.cy.jsx
+++ cypress/e2e/schood/schoolAdmin.cy.jsx
@@ -21,6 +21,90 @@ describe('School Admin', () => {
             expect(sessionStorage.getItem('token')).to.not.eq(null)
         })
         cy.url().should('eq', 'http://localhost:3000/');
+        cy.intercept('GET', 'http://localhost:3000/user/all', (req) => {
+            req.reply({
+              ok: true,
+              status: 200,
+              body: [
+                {
+                  firstname: 'laura',
+                  lastname: 'citron',
+                  email: 'laura@schood.fr',
+                  role: {
+                    _id: 1,
+                    name: 'teacher',
+                    levelOfAccess: 1
+                  },
+                  classes: {
+                    _id: 0,
+                    name: '200'
+                  }
+                },
+                {
+                  firstname: 'thomas',
+                  lastname: 'apple',
+                  email: 'thomas@schood.fr',
+                  role: {
+                    _id: 1,
+                    name: 'student',
+                    levelOfAccess: 1
+                  },
+                  classes: {
+                    _id: 0,
+                    name: '200'
+                  }
+                }
+              ]
+            });
+          }).as('getUsers');
+
+          cy.intercept('GET', 'http://localhost:3000/adm/classes', (req) => {
+            req.reply({
+              ok: true,
+              status: 200,
+              body: [
+                {
+                  _id: 0,
+                  name: '200',
+                  facility: '0'
+                },
+                {
+                  _id: 1,
+                  name: '201',
+                  facility: '0'
+                }
+              ]
+            });
+          }).as('getClasses');
+
+          cy.intercept('GET', 'http://localhost:3000/adm/rolesList', (req) => {
+            req.reply({
+              ok: true,
+              status: 200,
+              body: [
+                {
+                  _id: 0,
+                  name: 'student',
+                  levelOfAccess: 0
+                },
+                {
+                  _id: 1,
+                  name: 'teacher',
+                  levelOfAccess: 1
+                },
+                {
+                  _id: 2,
+                  name: 'administration',
+                  levelOfAccess: 2
+                },
+                {
+                  _id: 3,
+                  name: 'admin',
+                  levelOfAccess: 3
+                }
+              ]
+            });
+          }).as('getRoles');
         cy.get('#sidebar-item-1').click()
         cy.url().should('eq', 'http://localhost:3000/accounts');
 
@@ -36,37 +120,39 @@ describe('School Admin', () => {
         cy.get('@lastNameInput').should('have.value', 'Doe');
         cy.get('@emailInput').type('john.doe@schood.fr')
         cy.get('@emailInput').should('have.value', 'john.doe@schood.fr');
+        cy.get('#classe-select').select('200')
 
-        // const singleAccountCreationUrl = 'http://localhost:8080/adm/register'
-        // cy.intercept('POST', singleAccountCreationUrl, (req) => {
-        //     req.reply({
-        //       ok: true,
-        //       status: 200,
-        //       body: {},
-        //     });
-        //   }).as('getMock');
 
-        // cy.get('.account-submit-btn').click()
-        // cy.get('#err-message').should('have.text', 'Compte créé avec succès')
+        const singleAccountCreationUrl = 'http://localhost:8080/adm/register'
+        cy.intercept('POST', singleAccountCreationUrl, (req) => {
+            req.reply({
+              ok: true,
+              status: 200,
+              body: {},
+            });
+          }).as('postRegister');
 
-        // cy.get('.btn-close').click()
+        cy.get('.account-submit-btn').click()
+        cy.get('#err-message').should('have.text', 'Compte créé avec succès')
 
-        // //create many accounts
-        // cy.get('#many-account-btn').click()
+        cy.get('.btn-close').click()
 
-        // const manyAccountCreationUrl = 'http://localhost:8080/adm/csvRegisterUser'
-        // cy.intercept('POST', manyAccountCreationUrl, (req) => {
-        //     req.reply({
-        //       ok: true,
-        //       status: 200,
-        //       body: {},
-        //     });
-        //   }).as('getMock');
+        //create many accounts
+        cy.get('#many-account-btn').click()
 
-        // cy.get('.account-submit-btn').click()
-        // cy.get('#err-message').should('have.text', 'Compte(s) créé(s) avec succès')
+        const manyAccountCreationUrl = 'http://localhost:8080/adm/csvRegisterUser'
+        cy.intercept('POST', manyAccountCreationUrl, (req) => {
+            req.reply({
+              ok: true,
+              status: 200,
+              body: {},
+            });
+          }).as('postCsv');
 
-        // cy.get('.btn-close').click()
+        cy.get('.account-submit-btn').click()
+        cy.get('#err-message').should('have.text', 'Compte(s) créé(s) avec succès')
+
+        cy.get('.btn-close').click()
 
         // // check message page
         // cy.get('#sidebar-item-3').click()
--- cypress/e2e/schood/teacher.cy.jsx
+++ cypress/e2e/schood/teacher.cy.jsx
@@ -1,6 +1,89 @@
 /// <reference types="cypress" />
 
 describe('Teacher', () => {
+    const questionnaireUrl = 'http://localhost:3000/shared/questionnaire/123'
+    const answerListUrl = 'http://localhost:3000/teacher/questionnaire/123/answers/64f72b4e06c0818813902624'
+    const studentListUrl = 'http://localhost:3000/teacher/questionnaire/123/students'
+
+    const questionnaireResponse = {
+        _id: '123',
+        classes: [{ _id: '64f72b4d06c0818813902612', name: '200' }],
+        createdBy: {
+        _id: '64f72b4e06c0818813902620',
+        email: 'teacher1@schood.fr',
+        firstname: 'teacher1',
+        lastname: 'teacher1'
+        },
+        questions: [
+        {
+            _id: '64fb230269a0b02380ee32a7',
+            answers: [],
+            title: 'Comment te sens-tu à propos de ce test ?',
+            type: 'emoji'
+        },
+        {
+            _id: '64fb230269a0b02380ee32a8',
+            answers: [],
+            title: 'Elabores sur ta réponse à la question précédente',
+            type: 'text'
+        },
+        {
+            _id: '64fb230269a0b02380ee32a9',
+            answers: [
+            {
+                _id: '64fb230269a0b02380ee32ab',
+                position: 0,
+                title: 'oui'
+            },
+            {
+                _id: '64fb230269a0b02380ee32ac',
+                position: 1,
+                title: 'non'
+            }
+            ],
+            title: 'Est-ce que le texte fonctionne ?',
+            type: 'multiple'
+        }
+        ],
+        fromDate: '2023-09-03T00:00:00.000Z',
+        title: 'Questionnaire test',
+        toDate: '2023-09-09T00:00:00.000Z'
+    }
+
+    const studentsResponse = [
+        {
+        _id: 1,
+        users: [
+            {
+            _id: '64f72b4e06c0818813902624',
+            classes: ['64f72b4d06c0818813902612'],
+            email: 'student1@schood.fr',
+            firstname: 'student1',
+            lastname: 'student1'
+            }
+        ]
+        }
+    ]
+
+    const answersResponse = [
+        {
+        _id: '64fb242c19b9a22c8a0cde63',
+        answers: [
+            { question: '64fb230269a0b02380ee32a7', answer: '1', _id: '64fb242c19b9a22c8a0cde64' },
+            { question: '64fb230269a0b02380ee32a8', answer: 'Bof', _id: '64fb242c19b9a22c8a0cde65' },
+            { question: '64fb230269a0b02380ee32a9', answer: '0', _id: '64fb242c19b9a22c8a0cde66' }
+        ],
+        createdBy: {
+            _id: '64f72b4e06c0818813902624',
+            email: 'student1@schood.fr',
+            firstname: 'student1',
+            lastname: 'student1'
+        },
+        date: '2023-09-08T00:00:00.000Z',
+        questionnaire: '123'
+        }
+    ]
+
     beforeEach(() => {
         cy.visit('http://localhost:3000/login')
     })
@@ -54,7 +137,7 @@ describe('Teacher', () => {
 
         cy.get('#parution-date').invoke('val', '2025-10-15')
 
-        const formUrl = 'http://localhost:8080//teacher/questionnaire'
+        const formUrl = 'http://localhost:8080/teacher/questionnaire'
         cy.intercept('POST', formUrl, (req) => {
             req.reply({
               ok: true,
@@ -78,7 +161,22 @@ describe('Teacher', () => {
         cy.url().should('eq', 'http://localhost:3000/questionnaires')
         cy.contains('This is a test').should('exist');
 
+        cy.intercept('GET', questionnaireUrl, (req) => {
+            req.reply({ body: questionnaireResponse });
+        }).as('fetchForm');
+
+        cy.intercept('GET', studentListUrl, (req) => {
+            req.reply({ body: studentsResponse });
+        }).as('fetchStudents');
+
+        cy.intercept('GET', answerListUrl, (req) => {
+            req.reply({ body: answersResponse });
+        }).as('fetchAnswers');
+
+        cy.get('#access-btn-0').click()
+        cy.url().should('eq', 'http://localhost:3000/questionnaire/123')
+
         cy.get('#logout-button').click()
-        cy.url().should('eq', 'http://localhost:3000/login');
+        cy.url().should('eq', 'http://localhost:3000/login')
     })
 })
