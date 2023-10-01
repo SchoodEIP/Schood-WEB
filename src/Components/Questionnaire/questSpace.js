52e57e84ca3302f1a320f7d5e8335cec1ad753e7
Leo-Nord <eleonore.wichegrod@epitech.eu>
2023-10-01 16:30:14 +0100

    [ADD] questionnaire status

--- src/Components/Questionnaire/questSpace.js
+++ src/Components/Questionnaire/questSpace.js
@@ -1,7 +1,32 @@
-import { React } from 'react'
-import '../../css/Components/Questionnaire/questSpace.css'
+import React, { useEffect, useState } from 'react';
+import '../../css/Components/Questionnaire/questSpace.css';
 
-export function QuestSpace () {
+export function QuestSpace() {
+  const [previousQuestStatus, setPreviousQuestStatus] = useState(''); // Statut du questionnaire précédent
+  const [currentQuestStatus, setCurrentQuestStatus] = useState(''); // Statut du questionnaire hebdomadaire
+
+  useEffect(() => {
+    // Effectuer une requête GET pour récupérer le statut du questionnaire précédent
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/shared/questionnaire/previous`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPreviousQuestStatus(data.status);
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération du statut du questionnaire précédent :', error);
+      });
+
+    // Effectuer une requête GET pour récupérer le statut du questionnaire hebdomadaire
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/shared/questionnaire/current`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCurrentQuestStatus(data.status);
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération du statut du questionnaire hebdomadaire :', error);
+      });
+  }, []);
+
   return (
     <div className='quest-box'>
       <div className='quest-header'>
@@ -9,9 +34,62 @@ export function QuestSpace () {
       </div>
       <div className='quest-body'>
         <div className='quest-content'>
-          <p>Vous n'avez pas de questionnaire.</p>
+          <div className='quest-previous'>
+            <p>Questionnaire précédent</p>
+            {previousQuestStatus === 'not_started' && (
+              <div className='quest-start'>
+                <button className='green-button' onClick={() => window.location.href = '/questionnaires'}>
+                  Lancer le questionnaire
+                </button>
+              </div>
+            )}
+            {previousQuestStatus === 'in_progress' && (
+              <div className='quest-start'>
+                Ce questionnaire a été commencé.
+              </div>
+            )}
+            {previousQuestStatus === 'completed' && (
+              <div className='quest-start'>
+                Ce questionnaire est fini.
+              </div>
+            )}
+            {previousQuestStatus === 'completed' && (
+              <div className='quest-terminate'>
+                <button className='orange-button' onClick={() => window.location.href = '/questionnaires'}>
+                  Terminer le questionnaire
+                </button>
+              </div>
+            )}
+          </div>
+          <div className='quest-current'>
+            <p>Questionnaire hebdomadaire</p>
+            {currentQuestStatus === 'not_started' && (
+              <div className='quest-start'>
+                <button className='green-button' onClick={() => window.location.href = '/questionnaires'}>
+                  Lancer le questionnaire
+                </button>
+              </div>
+            )}
+            {currentQuestStatus === 'in_progress' && (
+              <div className='quest-start'>
+                Ce questionnaire a été commencé.
+              </div>
+            )}
+            {currentQuestStatus === 'completed' && (
+              <div className='quest-start'>
+                Ce questionnaire est fini.
+              </div>
+            )}
+            {currentQuestStatus === 'not_started' && (
+              <div className='quest-terminate'>
+                <button className='orange-button' onClick={() => window.location.href = '/questionnaires'}>
+                  Terminer le questionnaire
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
