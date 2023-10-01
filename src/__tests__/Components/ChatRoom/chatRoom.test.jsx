114bc661a95e5b43b68fbc1f119f6948d53a216c
Leo-Nord <eleonore.wichegrod@epitech.eu>
2023-10-01 22:40:48 +0100

    [ADD] tests chatRoom

--- src/__tests__/Components/ChatRoom/chatRoom.test.jsx
+++ src/__tests__/Components/ChatRoom/chatRoom.test.jsx
@@ -232,5 +232,59 @@ describe('Messages Component', () => {
       const errorMessage = screen.queryByText("Erreur lors de l'envoi du message. Veuillez réessayer.")
       expect(errorMessage).not.toBeInTheDocument()
     })
-  })
+  })  
+
+  it('creates a new conversation', async () => {
+    await act(async () => {
+      render(<Messages />);
+    });
+  
+    // Trigger the creation of a new conversation (you may need to add a button or UI element for this)
+    // Ensure that the new conversation appears in the conversations list
+  
+    await waitFor(() => {
+      const newConversationButton = screen.getByText('Nouvelle conversation', { selector: 'button' });
+      expect(newConversationButton).toBeInTheDocument();
+    });    
+  });
+
+  it('sets file type to "pdf" for a PDF file', async () => {
+    await act(async () => {
+      render(<Messages />);
+    });
+  
+    // Simulate selecting a PDF file
+    const fileInput = screen.getByLabelText('+');
+    fireEvent.change(fileInput, { target: { files: [new File([], 'test.pdf')] } });
+  
+    // Mock a failed fetch request
+    const mockFetch = jest.fn().mockRejectedValue(new Error('Failed to send message'))
+
+    global.fetch = mockFetch
+
+    // Click the "Envoyer" button to send the message
+    await act(async () => {
+      fireEvent.click(screen.getByText('Envoyer'))
+    })
+  });
+  
+  it('sets file type to "other" for a other file', async () => {
+    await act(async () => {
+      render(<Messages />);
+    });
+  
+    // Simulate selecting a PDF file
+    const fileInput = screen.getByLabelText('+');
+    fireEvent.change(fileInput, { target: { files: [new File([], 'test.other')] } });
+  
+    // Mock a failed fetch request
+    const mockFetch = jest.fn().mockRejectedValue(new Error('Failed to send message'))
+
+    global.fetch = mockFetch
+
+    // Click the "Envoyer" button to send the message
+    await act(async () => {
+      fireEvent.click(screen.getByText('Envoyer'))
+    })
+  });  
 })
