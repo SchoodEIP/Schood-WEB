7fe42bb116b22678cbbb269088d995eb20a670dc
Leo-Nord <Leo-Nord@users.noreply.github.com>
2023-10-01 10:29:21 +0000

    [LINT] Applied linter fix

--- src/__tests__/Components/Aides/aides.test.jsx
+++ src/__tests__/Components/Aides/aides.test.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen, fireEvent, act } from '@testing-library/react';
-import AidePage from '../../../Components/Aides/aides';
+import React from 'react'
+import { render, screen, fireEvent, act } from '@testing-library/react'
+import AidePage from '../../../Components/Aides/aides'
 
 // Mock de useEffect pour éviter de faire des appels HTTP réels
-jest.spyOn(React, 'useEffect').mockImplementation((f) => f());
+jest.spyOn(React, 'useEffect').mockImplementation((f) => f())
 
 describe('AidePage component', () => {
   // it('renders without errors', () => {
@@ -13,29 +13,29 @@ describe('AidePage component', () => {
   // });
 
   it('displays categories and contacts', () => {
-    render(<AidePage />);
+    render(<AidePage />)
 
     // Vérifie que les catégories sont affichées
-    expect(screen.getByText('Catégories')).toBeTruthy();
-    expect(screen.getByText('Harcèlement')).toBeTruthy();
-    expect(screen.getByText('Problème à la maison')).toBeTruthy();
+    expect(screen.getByText('Catégories')).toBeTruthy()
+    expect(screen.getByText('Harcèlement')).toBeTruthy()
+    expect(screen.getByText('Problème à la maison')).toBeTruthy()
 
     // Vérifie que les numéros de contact sont affichés
     // expect(screen.getByText('Numéros de Contact')).toBeInTheDocument();
-    expect(screen.queryByText('Aide contre le harcèlement')).toBeInTheDocument();
-    expect(screen.queryByText('Ligne d\'urgence pour les victimes de violence familiale')).toBeInTheDocument();
-  });
+    expect(screen.queryByText('Aide contre le harcèlement')).toBeInTheDocument()
+    expect(screen.queryByText('Ligne d\'urgence pour les victimes de violence familiale')).toBeInTheDocument()
+  })
 
   it('filters contacts when a category is clicked', async () => {
-    render(<AidePage />);
+    render(<AidePage />)
 
     // Clique sur la catégorie "Harcèlement"
     await act(async () => {
-      fireEvent.click(screen.getByText('Harcèlement'));
+      fireEvent.click(screen.getByText('Harcèlement'))
     })
 
     // Vérifie que seuls les contacts de la catégorie "Harcèlement" sont affichés
-    expect(screen.queryByText('Aide contre le harcèlement')).toBeInTheDocument();
-    expect(screen.queryByText('Ligne d\'urgence pour les victimes de violence familiale')).not.toBeInTheDocument();
-  });
-});
+    expect(screen.queryByText('Aide contre le harcèlement')).toBeInTheDocument()
+    expect(screen.queryByText('Ligne d\'urgence pour les victimes de violence familiale')).not.toBeInTheDocument()
+  })
+})
