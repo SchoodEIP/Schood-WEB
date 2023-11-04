04a762d240777ab011ff6939079b4af600d4fa6f
Leo-Nord <eleonore.wichegrod@epitech.eu>
2023-11-04 02:49:38 +0000

    [ADD] Signal option

---
+++ src/Components/ChatRoom/reportButton.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+const ReportButton = ({ currentConversation }) => {
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [reason, setReason] = useState('');
+  const [error, setError] = useState('');
+
+  const handleReportClick = () => {
+    setShowConfirmation(true);
+  };
+
+  const handleReasonChange = (e) => {
+    setReason(e.target.value);
+  };
+
+  const handleConfirmClick = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/user/chat/report`, {
+        method: 'POST',
+        headers: {
+          'x-auth-token': sessionStorage.getItem('token'),
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          conversationId: currentConversation._id,
+          reason: reason,
+        }),
+      });
+
+      if (response.status === 200) {
+        setShowConfirmation(false);
+        setReason('');
+      } else {
+        setError('Erreur lors du signalement de la conversation.');
+      }
+    } catch (error) {
+      setError('Erreur lors du signalement de la conversation.');
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleReportClick}>Signaler</button>
+      {showConfirmation && (
+        <div>
+          <select value={reason} onChange={handleReasonChange}>
+            <option value="">Sélectionnez une raison</option>
+            <option value="Contenu offensant">Contenu offensant</option>
+            <option value="Spam">Spam</option>
+            <option value="Autre">Autre</option>
+          </select>
+          <button onClick={handleConfirmClick}>Confirmer le signalement</button>
+          {error && <div className="error-message">{error}</div>}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ReportButton;
