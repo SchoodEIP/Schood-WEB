76c98c154eaeeddb3f4957f32cfd9dff87c37218
adbusnel <adrien.busnel@epitech.eu>
2024-01-05 13:22:43 +0100

    [ADD] show alerts

--- src/Components/Alerts/lastAlerts.js
+++ src/Components/Alerts/lastAlerts.js
@@ -1,7 +1,28 @@
-import { React } from 'react'
+import { React, useState, useEffect } from 'react'
 import '../../css/Components/Alerts/lastAlerts.css'
 
 export function LastAlerts () {
+  const [errMessage, setErrMessage] = useState('')
+  const [alerts, setAlerts] = useState([])
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/shared/alert/`, {
+      method: 'GET',
+      headers: {
+        'x-auth-token': sessionStorage.getItem('token')
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.length > 0) {
+          setAlerts(data)
+        }
+      })
+      .catch((error) => {
+        setErrMessage('Erreur : ', error)
+      })
+  }, [])
+
   return (
     <div className='alert-box'>
       <div className='alert-header'>
@@ -9,7 +30,11 @@ export function LastAlerts () {
       </div>
       <div className='alert-body'>
         <div className='alert-content'>
-          <p>Vous n'avez pas de nouvelle alerte.</p>
+          {errMessage !== '' ? <p>{errMessage}</p> : ''}
+          {alerts ?
+            <p>Vous n'avez pas de nouvelle alerte.</p> :
+            <p>Il y a des alertes</p>
+          }
         </div>
       </div>
     </div>
