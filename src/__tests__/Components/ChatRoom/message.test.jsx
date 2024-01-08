a6d44fb14f75daabbb119fda914016a1018ea767
Eleonore Wichegrod <leo@localhost-live.home>
2024-01-08 10:27:59 +0100

    [FIX] linter

--- src/__tests__/Components/ChatRoom/message.test.jsx
+++ src/__tests__/Components/ChatRoom/message.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, act, waitFor } from '@testing-library/react'
+import { render, screen, act } from '@testing-library/react'
 import Messages from '../../../Components/ChatRoom/message'
 import '@testing-library/jest-dom/'
 
