971ee36c084e04dda9cb0d9d601c75009d534a67
Leo-Nord <Leo-Nord@users.noreply.github.com>
2024-01-08 13:19:26 +0000

    [LINT] Applied linter fix

--- src/__tests__/Users/Student/statisticsStudent.test.jsx
+++ src/__tests__/Users/Student/statisticsStudent.test.jsx
@@ -3,23 +3,23 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import StudentStatPage from '../../../Users/Student/statisticsStudent'
 
-jest.mock('../../../Components/Header/headerComp', () => () => <div data-testid="header-comp" />);
-jest.mock('../../../Components/Sidebar/sidebar', () => () => <div data-testid="sidebar" />);
+jest.mock('../../../Components/Header/headerComp', () => () => <div data-testid='header-comp' />)
+jest.mock('../../../Components/Sidebar/sidebar', () => () => <div data-testid='sidebar' />)
 
 describe('StudentStatPage Component', () => {
   it('renders without crashing', () => {
-    render(<StudentStatPage />);
-  });
+    render(<StudentStatPage />)
+  })
 
   it('renders HeaderComp', () => {
-    const { getByTestId } = render(<StudentStatPage />);
-    const headerComp = getByTestId('header-comp');
-    expect(headerComp).toBeInTheDocument();
-  });
+    const { getByTestId } = render(<StudentStatPage />)
+    const headerComp = getByTestId('header-comp')
+    expect(headerComp).toBeInTheDocument()
+  })
 
   it('renders Sidebar', () => {
-    const { getByTestId } = render(<StudentStatPage />);
-    const sidebar = getByTestId('sidebar');
-    expect(sidebar).toBeInTheDocument();
-  });
-});
+    const { getByTestId } = render(<StudentStatPage />)
+    const sidebar = getByTestId('sidebar')
+    expect(sidebar).toBeInTheDocument()
+  })
+})
