be00030faa8886ec68c6c8b17d5cd95047de7939
adbusnel <adrien.busnel@epitech.eu>
2024-01-09 16:54:20 +0100

    [MOD] showing alerts with file links

--- src/Components/Alerts/lastAlerts.js
+++ src/Components/Alerts/lastAlerts.js
@@ -5,14 +5,10 @@ import moment from 'moment'
 export function LastAlerts () {
   const [errMessage, setErrMessage] = useState('')
   const [alerts, setAlerts] = useState([])
-  // const [userList, setUserList] = useState([])
 
   useEffect(() => {
-    // const findUserById = (id) => {
-    //     return userList.find(user => user._id === id);
-    // };
 
-    async function getFile (id) {
+    async function getFile(id) {
       try {
         const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/user/file/${id}`, {
           method: 'GET',
@@ -23,7 +19,7 @@ export function LastAlerts () {
         if (response.status !== 200) {
           throw new Error('Erreur lors de la réception du fichier.')
         } else {
-          const blob = response.blob()
+          const blob = await response.blob()
           const objectURL = URL.createObjectURL(blob)
           return objectURL
         }
@@ -32,47 +28,24 @@ export function LastAlerts () {
       }
     }
 
-    function buildList (dataList) {
+    async function buildList(dataList) {
       const alertList = []
-      dataList.forEach((data, index) => {
-        let fileUrl = ''
+      for (const data of dataList) {
+        let fileUrl = "";
         if (data.file) {
-          getFile(data.file).then((data) => {
-            fileUrl = data
-          })
-            .catch((error) => {
-              console.error('Erreur lors de la récupération du fichier :', error)
-            })
+          fileUrl = await getFile(data.file);
         }
-
         const showAlert = {
           id: data._id,
           title: data.title,
           message: data.message,
           file: fileUrl,
           createdAt: moment(data.createdAt).format('DD/MM/YYYY')
-          // createdBy: findUserById(data.createdBy),
         }
         alertList.push(showAlert)
-      })
-      console.log(alertList)
+      }
       return alertList
     }
-
-    // fetch(`${process.env.REACT_APP_BACKEND_URL}/user/all/`, {
-    //     method: 'GET',
-    //     headers: {
-    //       'x-auth-token': sessionStorage.getItem('token'),
-    //       'Content-Type': 'application/json'
-    //     }
-    //   })
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         setUserList(data)
-    //     })
-    //     .catch((error) => {
-    //         setErrMessage('Erreur : ', error)
-    //     })
 
     fetch(`${process.env.REACT_APP_BACKEND_URL}/shared/alert/`, {
       method: 'GET',
@@ -83,30 +56,8 @@ export function LastAlerts () {
     })
       .then((response) => response.json())
       .then((data) => {
-        // const placeholderList = [
-        //   {
-        //     title: 'Première Alerte',
-        //     message: 'Ceci est la première alerte',
-        //     classes: [],
-        //     role: [],
-        //     createdAt: '2023',
-        //     createdBy: '0921',
-        //     file: '',
-        //     _id: '123'
-        //   },
-        //   {
-        //     title: 'Mr Math',
-        //     message: 'Des contacts pour le soutien scolaire se trouvent dans la partie aide',
-        //     classes: [],
-        //     role: [],
-        //     createdAt: '2023',
-        //     createdBy: '0921',
-        //     file: 'math_file',
-        //     _id: '132'
-        //   }
-        // ]
-        // setAlerts(buildList(placeholderList))
-        setAlerts(buildList(data))
+        const promisedList = buildList(data)
+        promisedList.then((alertList) => setAlerts(alertList))
       })
       .catch((error) => {
         setErrMessage('Erreur : ', error.message)
@@ -128,10 +79,9 @@ export function LastAlerts () {
                   <div key={index} className='alert-container'>
                     <div className='alert-title'>{alert.title}</div>
                     <div className='alert-message'>{alert.message}</div>
-                    {console.log(alert)}
                     {alert.file
                       ? (<div className='alert-file-btn' id={alert.id}>
-                        <a href={alert.file} target='_blank' rel='noopener noreferrer'>
+                        <a style={{textDecoration:"none", color:"white"}} href={alert.file} target='_blank' rel='noopener noreferrer'>
                           Télécharger le fichier
                         </a>
                       </div>)
