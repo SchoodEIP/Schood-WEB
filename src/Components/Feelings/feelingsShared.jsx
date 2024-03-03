a6848c4391d14b133f9f5302ac8971313477f8cc
Leo-Nord <Leo-Nord@users.noreply.github.com>
2024-03-03 12:41:22 +0000

    [LINT] Applied linter fix

--- src/Components/Feelings/feelingsShared.jsx
+++ src/Components/Feelings/feelingsShared.jsx
@@ -2,40 +2,40 @@ import React, { useState, useEffect } from 'react'
 import '../../css/Components/Feelings/feelings.scss'
 
 const emotionsData = [
-    { emotion: 'Joyeux', description: 'Heureux et enthousiaste', emoji: '😄' },
-    { emotion: 'Joie modérée', description: 'Content et satisfait', emoji: '😊' },
-    { emotion: 'Plein d\'énergie', description: 'Énergique et dynamique', emoji: '💪' },
-    { emotion: 'Calme', description: 'Tranquille et serein', emoji: '😌' },
-    { emotion: 'Neutre', description: 'Ni joyeux ni triste', emoji: '😐' },
-    { emotion: 'Ennuyé', description: 'Peu intéressé et ennuyé', emoji: '😑' },
-    { emotion: 'Triste', description: 'Attristé et déprimé', emoji: '😢' },
-    { emotion: 'En colère', description: 'Irrité et furieux', emoji: '😡' },
-    { emotion: 'Fatigué', description: 'Fatigué mais pas épuisé', emoji: '😓' },
-    { emotion: 'Exténué', description: 'Épuisé et vidé', emoji: '😩' },
-    { emotion: 'Anxieux', description: 'Inquiet et nerveux', emoji: '😰' },
-    { emotion: 'Sous pression', description: 'Sous stress et pression', emoji: '🤯' },
-    { emotion: 'Malade', description: 'Malade et faible', emoji: '🤢' },
-    { emotion: 'Perdu', description: 'Confus et désorienté', emoji: '😵' },
+  { emotion: 'Joyeux', description: 'Heureux et enthousiaste', emoji: '😄' },
+  { emotion: 'Joie modérée', description: 'Content et satisfait', emoji: '😊' },
+  { emotion: 'Plein d\'énergie', description: 'Énergique et dynamique', emoji: '💪' },
+  { emotion: 'Calme', description: 'Tranquille et serein', emoji: '😌' },
+  { emotion: 'Neutre', description: 'Ni joyeux ni triste', emoji: '😐' },
+  { emotion: 'Ennuyé', description: 'Peu intéressé et ennuyé', emoji: '😑' },
+  { emotion: 'Triste', description: 'Attristé et déprimé', emoji: '😢' },
+  { emotion: 'En colère', description: 'Irrité et furieux', emoji: '😡' },
+  { emotion: 'Fatigué', description: 'Fatigué mais pas épuisé', emoji: '😓' },
+  { emotion: 'Exténué', description: 'Épuisé et vidé', emoji: '😩' },
+  { emotion: 'Anxieux', description: 'Inquiet et nerveux', emoji: '😰' },
+  { emotion: 'Sous pression', description: 'Sous stress et pression', emoji: '🤯' },
+  { emotion: 'Malade', description: 'Malade et faible', emoji: '🤢' },
+  { emotion: 'Perdu', description: 'Confus et désorienté', emoji: '😵' }
 ]
 
 const Feelings = () => {
-  const [selectedEmotion, setSelectedEmotion] = useState(''); // Émotion sélectionnée
-  const [writtenFeeling, setWrittenFeeling] = useState(''); // Ressenti écrit
-  const [isAnonymous, setIsAnonymous] = useState(true); // Anonyme par défaut
+  const [selectedEmotion, setSelectedEmotion] = useState('') // Émotion sélectionnée
+  const [writtenFeeling, setWrittenFeeling] = useState('') // Ressenti écrit
+  const [isAnonymous, setIsAnonymous] = useState(true) // Anonyme par défaut
   const [alertResponse, setAlertResponse] = useState('')
   const [showPopup, setShowPopup] = useState(false)
-  
+
   const handleEmotionClick = (emotion) => {
-    setSelectedEmotion(emotion);
+    setSelectedEmotion(emotion)
   }
   // Fonction pour gérer l'envoi du ressenti
   const sendFeelings = () => {
     // Construire l'objet de données à envoyer
     const feelingsData = {
       emotion: selectedEmotion,
-      writtenFeeling: writtenFeeling,
-      isAnonymous: isAnonymous
-    };
+      writtenFeeling,
+      isAnonymous
+    }
 
     // Effectuer la requête POST
     fetch(`${process.env.REACT_APP_BACKEND_URL}/path/to/feelings`, {
@@ -48,71 +48,73 @@ const Feelings = () => {
     })
       .then(response => {
         resetForm()
-        setAlertResponse('Ressenti envoyé avec succès');
+        setAlertResponse('Ressenti envoyé avec succès')
         setShowPopup(true)
       })
       .catch(error => {
-        setAlertResponse('Erreur lors de l\'envoi du ressenti', error);
+        setAlertResponse('Erreur lors de l\'envoi du ressenti', error)
         setShowPopup(true)
-      });
-  };
+      })
+  }
 
   useEffect(() => {
     if (showPopup) {
-      resetForm();
+      resetForm()
     }
   }, [showPopup])
 
   const resetForm = () => {
     console.log(writtenFeeling)
-   setWrittenFeeling('')
-   console.log(writtenFeeling)
-   setSelectedEmotion('')
-   setIsAnonymous(true)
+    setWrittenFeeling('')
+    console.log(writtenFeeling)
+    setSelectedEmotion('')
+    setIsAnonymous(true)
 
-   setTimeout(() => {
-    setAlertResponse('');
-    setShowPopup(false);
-   }, 3000)
-  };
+    setTimeout(() => {
+      setAlertResponse('')
+      setShowPopup(false)
+    }, 3000)
+  }
 
   return (
-    <div className="feelings-page">
+    <div className='feelings-page'>
       <div>
         <label>Sélectionnez votre émotion:</label>
-        <div className="emotion-grid">
-            {emotionsData.map((emotionData, index) => (
-              <div key={index} onClick={() => handleEmotionClick(emotionData.emotion)}
-                className={selectedEmotion === emotionData.emotion ? 'emoji-mood selected-emotion' : 'emoji-mood'}>
-                    <span role="img" aria-label={emotionData.emotion}>{emotionData.emoji}</span>
-                    <div className="emotion-name">{emotionData.emotion}</div>
-              </div>
-            ))}
+        <div className='emotion-grid'>
+          {emotionsData.map((emotionData, index) => (
+            <div
+              key={index} onClick={() => handleEmotionClick(emotionData.emotion)}
+              className={selectedEmotion === emotionData.emotion ? 'emoji-mood selected-emotion' : 'emoji-mood'}
+            >
+              <span role='img' aria-label={emotionData.emotion}>{emotionData.emoji}</span>
+              <div className='emotion-name'>{emotionData.emotion}</div>
+            </div>
+          ))}
         </div>
       </div>
 
       {/* Champ pour indiquer le ressenti par écrit */}
       <div>
-        <label htmlFor="feelingText">Indiquez votre ressenti par écrit:</label>
-        <textarea id="feelingText" data-testid="feelingText" value={writtenFeeling} onChange={(e) => setWrittenFeeling(e.target.value)} />
+        <label htmlFor='feelingText'>Indiquez votre ressenti par écrit:</label>
+        <textarea id='feelingText' data-testid='feelingText' value={writtenFeeling} onChange={(e) => setWrittenFeeling(e.target.value)} />
       </div>
 
       {/* Option pour répondre anonymement */}
       <div>
-        <label htmlFor="anonymousCheckbox" data-testid="anonymousCheckbox">Je souhaite rester anonyme:</label>
+        <label htmlFor='anonymousCheckbox' data-testid='anonymousCheckbox'>Je souhaite rester anonyme:</label>
         <input
-          id="anonymousCheckbox"
-          type="checkbox"
+          id='anonymousCheckbox'
+          type='checkbox'
           checked={isAnonymous}
           onChange={() => setIsAnonymous(!isAnonymous)}
         />
       </div>
 
-      {showPopup && <div data-testid="popupTest" className='popup' onClick={() => setShowPopup(false)}>{alertResponse}</div>}
-      <button data-testid="buttonSend" className="button-css" onClick={sendFeelings}>Envoyer le ressenti</button>
-      
-    </div>
-  );
-};
+      {showPopup && <div data-testid='popupTest' className='popup' onClick={() => setShowPopup(false)}>{alertResponse}</div>}
+      <button data-testid='buttonSend' className='button-css' onClick={sendFeelings}>Envoyer le ressenti</button>
 
-export default Feelings;
+    </div>
+  )
+}
+
+export default Feelings
--- src/__tests__/Components/Feelings/feelingsShared.test.jsx
+++ src/__tests__/Components/Feelings/feelingsShared.test.jsx
@@ -1,74 +1,74 @@
-import React from 'react';
-import { render, screen, act, fireEvent, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import Feelings from '../../../Components/Feelings/feelingsShared';
+import React from 'react'
+import { render, screen, act, fireEvent, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Feelings from '../../../Components/Feelings/feelingsShared'
 
-jest.useFakeTimers();
+jest.useFakeTimers()
 
 describe('Feelings Component', () => {
   it('renders without crashing', async () => {
     await act(async () => {
-      render(<Feelings />);
-    });
-    expect(screen.getByText('Joyeux')).toBeInTheDocument();
-  });
+      render(<Feelings />)
+    })
+    expect(screen.getByText('Joyeux')).toBeInTheDocument()
+  })
 
   it('selects emotion on click', async () => {
     await act(async () => {
-      render(<Feelings />);
-    });
+      render(<Feelings />)
+    })
 
-    const joyButton = screen.getByText('Joie modérée');
-    fireEvent.click(joyButton);
+    const joyButton = screen.getByText('Joie modérée')
+    fireEvent.click(joyButton)
 
-    expect(screen.getByText('Joie modérée').parentElement).toHaveClass('selected-emotion');
-  });
+    expect(screen.getByText('Joie modérée').parentElement).toHaveClass('selected-emotion')
+  })
 
   it('updates writtenFeeling on textarea change', async () => {
     await act(async () => {
-      render(<Feelings />);
-    });
-  
-    const textarea = screen.getByTestId('feelingText'); // Use 'feelingText' as the argument
-    fireEvent.change(textarea, { target: { value: 'Feeling test' } });
-  
-    expect(textarea).toHaveValue('Feeling test');
-  });
+      render(<Feelings />)
+    })
+
+    const textarea = screen.getByTestId('feelingText') // Use 'feelingText' as the argument
+    fireEvent.change(textarea, { target: { value: 'Feeling test' } })
+
+    expect(textarea).toHaveValue('Feeling test')
+  })
 
   it('toggles isAnonymous on checkbox change', async () => {
     await act(async () => {
-      render(<Feelings />);
-    });
-  
-    const checkbox = screen.getByTestId('anonymousCheckbox');
+      render(<Feelings />)
+    })
+
+    const checkbox = screen.getByTestId('anonymousCheckbox')
     await act(async () => {
-        await fireEvent.click(checkbox);
-    });
-  });
+      await fireEvent.click(checkbox)
+    })
+  })
 
   it('resets form state after popup is closed', async () => {
-    global.fetch = jest.fn(() => Promise.resolve({}));
-  
+    global.fetch = jest.fn(() => Promise.resolve({}))
+
     await act(async () => {
-      render(<Feelings />);
-    });
-  
+      render(<Feelings />)
+    })
+
     await act(async () => {
-      await fireEvent.click(screen.getByTestId('buttonSend'));
-    });
-  
-    expect(screen.getByText(/Ressenti envoyé avec succès/)).toBeInTheDocument();
-  
+      await fireEvent.click(screen.getByTestId('buttonSend'))
+    })
+
+    expect(screen.getByText(/Ressenti envoyé avec succès/)).toBeInTheDocument()
+
     act(() => {
-      jest.runAllTimers(); // Advance all timers
-    });
-  
+      jest.runAllTimers() // Advance all timers
+    })
+
     await waitFor(() => {
-      expect(screen.queryByTestId('popupTest')).not.toBeInTheDocument();
-    });
-  
-    expect(screen.getByLabelText('Indiquez votre ressenti par écrit:')).toHaveValue('');
-  
-    delete global.fetch;
-  });
-});
+      expect(screen.queryByTestId('popupTest')).not.toBeInTheDocument()
+    })
+
+    expect(screen.getByLabelText('Indiquez votre ressenti par écrit:')).toHaveValue('')
+
+    delete global.fetch
+  })
+})
