26f32e8ef968968d21788e9117dd5350fc68c939
Eléonore <eleonore.wichegrod@epitech.eu>
2024-03-03 12:29:16 +0000

    [ADD] Feeling form

--- src/Users/Shared/profilPage.jsx
+++ src/Users/Shared/profilPage.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Profil from '../../Components/Profil/profilPage'
 import HeaderComp from '../../Components/Header/headerComp'
 import Sidebar from '../../Components/Sidebar/sidebar'
+import Feelings from '../../Components/Feelings/feelingsShared'
 import '../../css/pages/profilPage.scss'
 
 const ProfilPage = () => {
+  const isStudent = sessionStorage.getItem('role') === 'student'
   return (
     <div>
       <div>
@@ -15,7 +17,10 @@ const ProfilPage = () => {
           <Sidebar />
         </div>
         <div className='left-half'>
-          <div><Profil /></div>
+          <div>
+            <Profil />
+            {isStudent && <Feelings />}
+          </div>
         </div>
       </div>
     </div>
