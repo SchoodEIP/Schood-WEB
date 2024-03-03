b27c9a72566eefcb0c4a7f82f3c32aaddba9295d
Exiels <Samina29@outlook.fr>
2024-03-03 23:34:42 +0100

    [FIX] Fixed linter

--- cypress/e2e/schood/admin.cy.jsx
+++ cypress/e2e/schood/admin.cy.jsx
@@ -1,27 +1,27 @@
 /// <reference types="cypress" />
 
 describe('Admin', () => {
-    before(() => {
-        cy.visit('http://localhost:3000/login')
-    })
+  before(() => {
+    cy.visit('http://localhost:3000/login')
+  })
 
-    after(() => {
-        cy.clearAllLocalStorage()
-        cy.clearAllSessionStorage()
-    })
+  after(() => {
+    cy.clearAllLocalStorage()
+    cy.clearAllSessionStorage()
+  })
 
-    it('checks admin part works', () => {
-        cy.get('#userInput').type('admin@schood.fr')
-        cy.get('#passInput').type('admin123')
-        cy.get('#submit-button').click()
-        cy.should(() => {
-            expect(localStorage.getItem('role')).to.eq('admin')
-            expect(sessionStorage.getItem('role')).to.eq('admin')
-            expect(localStorage.getItem('token')).to.not.eq(null)
-            expect(sessionStorage.getItem('token')).to.not.eq(null)
-        })
-        cy.url().should('eq', 'http://localhost:3000/');
-        cy.get('#sidebar-item-1').click()
-        cy.url().should('eq', 'http://localhost:3000/accounts');
+  it('checks admin part works', () => {
+    cy.get('#userInput').type('admin@schood.fr')
+    cy.get('#passInput').type('admin123')
+    cy.get('#submit-button').click()
+    cy.should(() => {
+      expect(localStorage.getItem('role')).to.eq('admin')
+      expect(sessionStorage.getItem('role')).to.eq('admin')
+      expect(localStorage.getItem('token')).to.not.eq(null)
+      expect(sessionStorage.getItem('token')).to.not.eq(null)
     })
+    cy.url().should('eq', 'http://localhost:3000/')
+    cy.get('#sidebar-item-1').click()
+    cy.url().should('eq', 'http://localhost:3000/accounts')
+  })
 })
