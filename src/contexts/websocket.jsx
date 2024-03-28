6a4d59e0d6e4f20da6e5f33e52716c93d1c75ec5
Exiels <Samina29@outlook.fr>
2024-03-29 00:33:55 +0100

    [ADD] Added fallback websocket url

--- src/contexts/websocket.jsx
+++ src/contexts/websocket.jsx
@@ -41,7 +41,7 @@ export const WebsocketProvider = ({ children }) => {
     const id = localStorage.getItem('id')
     if (!id) return
 
-    const socket = new WebSocket(process.env.REACT_APP_WS_URL) // eslint-disable-line
+    const socket = new WebSocket((process.env.REACT_APP_WS_URL || "ws://localhost:8080")) // eslint-disable-line
 
     socket.onopen = () => setIsReady(true)
     socket.onclose = () => setIsReady(false)
