e016a309be2aa6d146edafc3874ea8f003d91521
adbusnel <adrien.busnel@epitech.eu>
2024-03-30 17:55:46 +0100

    [MOD] removed send feelings component from profile page

--- src/Users/Shared/profilPage.jsx
+++ src/Users/Shared/profilPage.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import Profil from '../../Components/Profil/profilPage'
 import HeaderComp from '../../Components/Header/headerComp'
 import Sidebar from '../../Components/Sidebar/sidebar'
-import Feelings from '../../Components/Feelings/feelingsShared'
 import '../../css/pages/profilPage.scss'
 
 const ProfilPage = () => {
-  const isStudent = sessionStorage.getItem('role') === 'student'
   return (
     <div>
       <div>
@@ -19,7 +17,6 @@ const ProfilPage = () => {
         <div className='left-half'>
           <div>
             <Profil />
-            {isStudent && <Feelings />}
           </div>
         </div>
       </div>
