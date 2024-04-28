25854f732cb97dace2f5320f4677cdf193662f53
Exiels <Samina29@outlook.fr>
2024-04-28 06:48:37 +0200

    [MOD] Modified student form

--- src/Components/Header/headerComp.js
+++ src/Components/Header/headerComp.js
@@ -1,16 +1,38 @@
 import React from 'react'
 import logoSchood from '../../assets/logo_schood.png'
+import backButton from '../../assets/backButton.png'
 import '../../css/Components/Header/headerComp.css'
-import { Link } from "react-router-dom";
+import { Link, Router, useNavigate } from "react-router-dom";
 
-export default function HeaderComp ({title, withLogo = true}) {
+export default function HeaderComp ({title, withLogo = true, subtitle, withReturnBtn = false}) {
+  const navigate = useNavigate();
+  
+  const goBack = () => {
+    navigate(-1)
+  }
+
   return (
     <div id='header'>
-      {title && (
-        <div id='title'>
-          {title}
-        </div>
-      )}
+      <div id='left'>
+        {title && (
+          <div id='title'>
+            {title}
+          </div>
+        )}
+        {subtitle && (
+          <div id='subtitle'>
+            {subtitle}
+          </div>
+        )}
+        {withReturnBtn && (
+          <div id='withReturnBtn' onClick={goBack}>
+            <img id='return-btn' src={backButton} alt='Return'/>
+            <div id='back-text'>
+              Retour
+            </div>
+          </div>
+        )}
+      </div>
       {withLogo && (
         <Link to={'/'}>
           <img id='logo' src={logoSchood} alt='Schood'/>
