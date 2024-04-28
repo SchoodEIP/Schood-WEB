46ae3ccb78cc96e56c0e36368c9e42cbc62f0f9e
Exiels <Samina29@outlook.fr>
2024-04-28 22:33:40 +0200

    [MOD] Modified chat

--- src/Components/ChatRoom/chatRoomSidebar.jsx
+++ src/Components/ChatRoom/chatRoomSidebar.jsx
@@ -1,5 +1,6 @@
 import { WebsocketContext } from '../../contexts/websocket'
 import React, { useContext, useEffect } from 'react'
+import cross from "../../assets/cross2.png"
 
 const ChatRoomSidebar = ({
   conversations,
@@ -20,25 +21,27 @@ const ChatRoomSidebar = ({
   }
 
   return (
-    <div className='sidebar'>
-      <h2>Mes messages</h2>
-      <ul>
+    <div className='chat-sidebar'>
+      <div className='top'>
+        <button className='new-conversation-button' onClick={openCreateConversationPopup}>
+          <img className='img-cross' src={cross} alt="cross" />
+          <span>Nouvelle conversation</span>
+        </button>
+        <span className='divider'></span>
+      </div>
+      <div className='content'>
         {conversations.map((conversation, index) => (
-          <div
-            key={index}
-            className={`conversation ${conversation === currentConversation ? 'active-conversation' : ''}`}
-            onClick={() => handleClick(conversation)}
-          >{
-            (chats.value.unseenChats.includes(conversation._id)) &&
-              <div className='conversation-notification' />
-          }
-            {conversation.name}
+          <div key={index} className={`${conversation === currentConversation ? 'active-conversation' : 'conversation'}`} onClick={() => handleClick(conversation)} >
+            <div className='text'>
+              {conversation.name}
+            </div>
+            {
+              (chats.value.unseenChats.includes(conversation._id)) &&
+                <div className='conversation-notification' />
+            }
           </div>
         ))}
-      </ul>
-      <button className='new-conversation-button' onClick={openCreateConversationPopup}>
-        Nouvelle conversation
-      </button>
+      </div>
     </div>
   )
 }
