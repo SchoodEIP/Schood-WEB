7b49d28231d8432e37571b8ad3906a777a0ab099
Eléonore <eleonore.wichegrod@epitech.eu>
2024-04-29 09:38:00 +0100

    [FIX] statistics Student

--- src/Users/Student/statisticsStudent.jsx
+++ src/Users/Student/statisticsStudent.jsx
@@ -2,208 +2,176 @@ import React, { useState, useEffect } from 'react';
 import HeaderComp from '../../Components/Header/headerComp';
 import Sidebar from '../../Components/Sidebar/sidebar';
 import Chart from 'chart.js/auto';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../../css/pages/homePage.css';
-import '../../css/pages/statisticsStudent.scss'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSadTear, faFrown, faMeh, faSmile, faLaughBeam } from '@fortawesome/free-solid-svg-icons'
+import '../../css/pages/statisticsStudent.scss';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSadTear, faFrown, faMeh, faSmile, faLaughBeam } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faSadTear, faFrown, faMeh, faSmile, faLaughBeam)
+library.add(faSadTear, faFrown, faMeh, faSmile, faLaughBeam);
 
 const StudentStatPage = () => {
-  const [moodData, setMoodData] = useState([]); // État local pour stocker toutes les données d'humeur
-  const [filteredMoodData, setFilteredMoodData] = useState([]); // État local pour stocker les données filtrées
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]); // Initialiser avec la date actuelle
-  const [activeFilter, setActiveFilter] = useState([]); // État local pour suivre le filtre actif
+  const [moodData, setMoodData] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
+  const [activeFilter, setActiveFilter] = useState('Semaine');
+  const [chart, setChart] = useState(null)
 
   useEffect(() => {
-    const fetchData = async () => {
-      const questionnaireUrl = process.env.REACT_APP_BACKEND_URL + '/student/statistics/moods';
-      try {
-        const testData = [
-          { date: '2023-11-29', mood: 4 },
-          { date: '2024-03-29', mood: 0 },
-          { date: '2024-01-29', mood: 1 },
-          { date: '2024-04-03', mood: 4 },
-          { date: '2024-04-01', mood: 2 },
-          { date: '2024-04-02', mood: 1 },
-          { date: '2023-12-29', mood: 0 }, // Mauvaise humeur pour le 29/03/2024
-          { date: '2024-03-30', mood: 3 }, // Bonne humeur pour le 30/03/2024
-          { date: '2024-03-31', mood: 4 }  // Heureux pour le 31/03/2024
-        ];
-        // const response = await fetch(questionnaireUrl, {
-        //   method: 'GET',
-        //   headers: {
-        //     'x-auth-token': sessionStorage.getItem('token'),
-        //     'Content-Type': 'application/json'
-        //   }
-        // });
-        // const data = await response.json();
-        setMoodData(testData); // Mettre à jour l'état local avec les données d'humeur
-      } catch (error) {
-        console.error('Error fetching mood data:', error);
-      }
-    };
-
     fetchData();
-  }, []);
+  }, [selectedDate, activeFilter]); // Fetch data when selectedDate or activeFilter changes
 
   useEffect(() => {
-    // Filtrer les données en fonction de la date sélectionnée
-    const filteredData = moodData.filter(entry => entry.date === selectedDate);
-    setFilteredMoodData(filteredData);
-  }, [selectedDate, moodData]); // Appeler ce useEffect à chaque changement dans selectedDate ou moodData
+    if (chart) {
+      updateChart();
+    } else {
+      createChart();
+    }
+  }, [moodData])
 
-  useEffect(() => {
-    // Code pour afficher le graphique en utilisant Chart.js
-    if (filteredMoodData.length > 0) {
-      const sortedData = filteredMoodData.slice().sort((a, b) => new Date(a.date) - new Date(b.date));
-
-      const ctx = document.getElementById('moodChart').getContext('2d');
-      const chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: sortedData.map(entry => entry.date), // Utiliser les dates comme labels
-          datasets: [{
-            label: 'Évolution de l\'humeur',
-            data: sortedData.map(entry => entry.mood), // Utiliser les valeurs d'humeur comme données
-            borderColor: 'rgba(255, 255, 255, 1)',
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderWidth: 1
-          }]
+  const fetchData = async () => {
+    const moodUrl = process.env.REACT_APP_BACKEND_URL + '/student/statistics/dailyMoods';
+    try {
+      const response = await fetch(moodUrl, {
+        method: 'POST',
+        headers: {
+          'x-auth-token': sessionStorage.getItem('token'),
+          'Content-Type': 'application/json'
         },
-        options: {
-          responsive: true,
-          scales: {
-            x: {
-              title: {
-                display: true,
-                text: 'Date',
-                color: 'white'
-              }
-            },
-            y: {
-              title: {
-                display: true,
-                text: 'Humeur',
-                color: 'white'
+        body: JSON.stringify({
+          fromDate: calculateStartDate(selectedDate, activeFilter),
+          toDate: calculateEndDate(selectedDate, activeFilter)
+        })
+      });
+      const moodData = await response.json();
+      setMoodData(moodData);
+      console.log(moodData)
+    } catch (error) {
+      console.error('Error fetching mood data:', error);
+    }
+  };
+
+  const calculateStartDate = (date, filter) => {
+    const selectedDate = new Date(date);
+    switch (filter) {
+      case 'Semaine':
+        const selectedDayOfWeek = selectedDate.getDay();
+        const monday = new Date(selectedDate);
+        monday.setDate(monday.getDate() - selectedDayOfWeek + (selectedDayOfWeek === 0 ? -6 : 1));
+        return monday.toISOString().split('T')[0];
+      case 'Mois':
+        return new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1).toISOString().split('T')[0];
+      case 'Semestre':
+        const semesterStartMonth = selectedDate.getMonth() < 8 ? 0 : 8;
+        return new Date(selectedDate.getFullYear(), semesterStartMonth, 1).toISOString().split('T')[0];
+      case 'Année':
+        return new Date(selectedDate.getFullYear(), 0, 1).toISOString().split('T')[0];
+      default:
+        return selectedDate.toISOString().split('T')[0];
+    }
+  };
+
+  const calculateEndDate = (date, filter) => {
+    const selectedDate = new Date(date);
+    switch (filter) {
+      case 'Semaine':
+        const sunday = new Date(selectedDate);
+        sunday.setDate(sunday.getDate() - selectedDate.getDay() + 7);
+        return sunday.toISOString().split('T')[0];
+      case 'Mois':
+        const nextMonth = new Date(selectedDate);
+        nextMonth.setMonth(nextMonth.getMonth() + 1);
+        nextMonth.setDate(nextMonth.getDate() - 1);
+        return nextMonth.toISOString().split('T')[0];
+      case 'Semestre':
+        const semesterEndMonth = selectedDate.getMonth() < 8 ? 6 : 11;
+        const endMonth = new Date(selectedDate.getFullYear(), semesterEndMonth + 1, 0);
+        return endMonth.toISOString().split('T')[0];
+      case 'Année':
+        return new Date(selectedDate.getFullYear(), 11, 31).toISOString().split('T')[0];
+      default:
+        return selectedDate.toISOString().split('T')[0];
+    }
+  };
+
+  const createChart = () => {
+    const ctx = document.getElementById('moodChart');
+    const newChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: moodData.map(entry => entry.date),
+        datasets: [{
+          label: 'Humeur',
+          data: moodData.map(entry => entry.mood),
+          borderColor: 'white', // Couleur de la courbe
+          backgroundColor: 'white', // Couleur des points
+          tension: 0.1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            ticks: {
+              color: 'white', // Couleur de l'axe des ordonnées
+              callback: value => {
+                switch (value) {
+                  case 0:
+                    return '\u{1F622}';
+                  case 1:
+                    return '\u{1f641}';
+                  case 2:
+                    return '\u{1F610}';
+                  case 3:
+                    return '\u{1F603}';
+                  case 4:
+                    return '\u{1F604}';
+                  default:
+                    return '';
+                }
               },
-              ticks: {
-                callback: value => {
-                  switch (value) {
-                    case 0:
-                      return '\u{1F622}'
-                    case 1:
-                      return '\u{1f641}'
-                    case 2:
-                      return '\u{1F610}'
-                    case 3:
-                      return '\u{1F603}'
-                    case 4:
-                      return '\u{1F604}'
-                    default:
-                      return '';
-                  }
-                },
-                fontFamily: '"Font Awesome 5 Free"',
-                color: 'white'
-              }
+              fontFamily: '"Font Awesome 5 Free"'
             }
           },
-          plugins: {
-            legend: {
-              labels: {
-                color: 'white'
-              }
+          x: {
+            ticks: {
+              color: 'white' // Couleur de l'axe des abscisses
             }
-          },
-          layout: {
+          }
+        },
+        plugins: {
+          legend: {
+            labels: {
+              color: 'white' // Couleur de la légende
+            }
           }
         }
+      }
+    });
+    setChart(newChart);
+  };
+
+  const updateChart = () => {
+    if (moodData) {
+      const dates = Object.keys(moodData).filter(key => key !== 'averagePercentage');
+      const moods = Object.values(moodData).filter(val => typeof val === 'number');
+  
+      const data = dates.map(date => {
+        return {
+          x: date,
+          y: moodData[date],
+          r: 10 // Taille du point
+        };
       });
-      return () => {
-        chart.destroy(); // Nettoyer le graphique lors du démontage du composant
-      };
+  
+      chart.data.datasets[0].data = data;
+      chart.update();
     }
-  }, [filteredMoodData]); // Appeler ce useEffect à chaque changement dans filteredMoodData
+  }
 
   const handleDateChange = (event) => {
-    setSelectedDate(event.target.value); // Mettre à jour la date sélectionnée
-    setActiveFilter([]);
+    setSelectedDate(event.target.value);
   };
 
-  useEffect(() => {
-    // Appliquer le filtre par semaine et définir le bouton "Semaine" comme actif au montage du composant
-    filterByWeek();
-    setActiveFilter([]);
-  }, []); // Appeler cette fonction une seule fois au montage du composant
-
-  const filterByWeek = () => {
-    // Récupérer le jour de la semaine de la date sélectionnée (0 pour dimanche, 1 pour lundi, ..., 6 pour samedi)
-    const selectedDayOfWeek = new Date(selectedDate).getDay();
-    // Calculer la date du lundi précédent
-    const monday = new Date(selectedDate);
-    monday.setDate(monday.getDate() - selectedDayOfWeek + (selectedDayOfWeek === 0 ? -6 : 1));
-    const mondayDate = monday.toISOString().split('T')[0];
-
-    // Calculer la date du dimanche suivant
-    const sunday = new Date(mondayDate);
-    sunday.setDate(sunday.getDate() + 6);
-    const sundayDate = sunday.toISOString().split('T')[0];
-
-    // Filtrer les données pour la semaine du lundi précédent au dimanche suivant
-    const filteredData = moodData.filter(entry => entry.date >= mondayDate && entry.date <= sundayDate);
-    setFilteredMoodData(filteredData);
-    setActiveFilter('Semaine');
-  };
-
-  const filterByMonth = () => {
-    // Récupérer le mois de la date sélectionnée (de 0 à 11, où 0 représente janvier et 11 représente décembre)
-    const selectedMonth = new Date(selectedDate).getMonth();
-    // Récupérer l'année de la date sélectionnée
-    const selectedYear = new Date(selectedDate).getFullYear();
-
-    // Filtrer les données pour le mois de la date sélectionnée
-    const filteredData = moodData.filter(entry => {
-      const entryDate = new Date(entry.date);
-      return entryDate.getMonth() === selectedMonth && entryDate.getFullYear() === selectedYear;
-    });
-    setFilteredMoodData(filteredData);
-    setActiveFilter('Mois');
-  };
-
-  const filterBySemester = () => {
-    // Récupérer le mois de la date sélectionnée (de 0 à 11, où 0 représente janvier et 11 représente décembre)
-    const selectedMonth = new Date(selectedDate).getMonth();
-    // Récupérer l'année de la date sélectionnée
-    const selectedYear = new Date(selectedDate).getFullYear();
-
-    // Déterminer le semestre en fonction du mois
-    const semester = selectedMonth < 8 ? 1 : 2; // Premier semestre si le mois est de janvier à août, deuxième semestre sinon
-
-    // Filtrer les données pour le semestre de l'année scolaire correspondant à la date sélectionnée
-    const filteredData = moodData.filter(entry => {
-      const entryDate = new Date(entry.date);
-      const entryMonth = entryDate.getMonth();
-      const entryYear = entryDate.getFullYear();
-      const entrySemester = entryMonth < 8 ? 1 : 2; // Déterminer le semestre de chaque entrée
-      return entrySemester === semester && entryYear === selectedYear;
-    });
-    setFilteredMoodData(filteredData);
-    setActiveFilter(`Semestre ${semester}`);
-  };
-
-  const filterByYear = () => {
-    // Récupérer l'année scolaire correspondant à la date sélectionnée
-    const selectedYear = new Date(selectedDate).getMonth() < 8 ? new Date(selectedDate).getFullYear() - 1 : new Date(selectedDate).getFullYear();
-
-    // Filtrer les données pour l'année scolaire correspondant à la date sélectionnée
-    const filteredData = moodData.filter(entry => {
-      const entryYear = new Date(entry.date).getFullYear();
-      return entryYear === selectedYear || entryYear === selectedYear + 1;
-    });
-    setFilteredMoodData(filteredData);
-    setActiveFilter(`Année scolaire ${selectedYear}-${selectedYear + 1}`);
+  const handleFilterChange = (filter) => {
+    setActiveFilter(filter);
   };
 
   return (
@@ -212,18 +180,18 @@ const StudentStatPage = () => {
       <div className='page-content'>
         <Sidebar />
         <div>
-          <label htmlFor="dateFilter">Filtrer par date:</label>
+          <label htmlFor="dateFilter">Sélectionner une date:</label>
           <input type="date" id="dateFilter" value={selectedDate} onChange={handleDateChange} />
-          <button className={activeFilter === 'Semaine' ? 'active' : ''} onClick={filterByWeek}>Semaine</button>
-          <button className={activeFilter === 'Mois' ? 'active' : ''} onClick={filterByMonth}>Mois</button>
-          <button className={activeFilter.includes('Semestre') ? 'active' : ''} onClick={filterBySemester}>Semestre</button>
-          <button className={activeFilter.includes('Année') ? 'active' : ''} onClick={filterByYear}>Année</button>
+          <button className={activeFilter === 'Semaine' ? 'active' : ''} onClick={() => handleFilterChange('Semaine')}>Semaine</button>
+          <button className={activeFilter === 'Mois' ? 'active' : ''} onClick={() => handleFilterChange('Mois')}>Mois</button>
+          <button className={activeFilter === 'Semestre' ? 'active' : ''} onClick={() => handleFilterChange('Semestre')}>Semestre</button>
+          <button className={activeFilter === 'Année' ? 'active' : ''} onClick={() => handleFilterChange('Année')}>Année</button>
           <h1>Evolution de mon humeur</h1>
-          <canvas id="moodChart"></canvas> {/* Canvas pour afficher le graphique */}
+          <canvas id="moodChart"></canvas>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default StudentStatPage;
