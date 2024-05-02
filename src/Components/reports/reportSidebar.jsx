92bda7ee2e4e4f63f549fdb2c03119f166eb4f51
adbusnel <adrien.busnel@epitech.eu>
2024-05-02 14:51:47 +0200

    [ADD] Added report page

---
+++ src/Components/reports/reportSidebar.jsx
@@ -0,0 +1,33 @@
+import { WebsocketContext } from '../../contexts/websocket'
+import React, { useContext, useEffect } from 'react'
+import cross from "../../assets/cross2.png"
+
+const ReportSidebar = ({
+  reports,
+  currentReport,
+  setCurrentReport,
+  handleReportSelection
+}) => {
+  const handleClick = (report) => {
+    setCurrentReport(report)
+    if (report.conversation) {
+        handleReportSelection(report._id, report.conversation)
+    }
+  }
+
+  return (
+    <div className='chat-sidebar'>
+      <div className='content'>
+        {reports.map((report, index) => (
+          <div key={index} className={`${report === currentReport ? 'active-conversation' : 'conversation'}`} onClick={() => handleClick(report)} >
+            <div className='text'>
+              {report.signaledBy.lastname}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ReportSidebar
