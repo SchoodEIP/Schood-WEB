d5ac245291d1f4090259b7ea556b624c4c703932
adbusnel <adrien.busnel@epitech.eu>
2024-09-21 15:51:23 +0200

    [MOD] fixed modification of help numbers and their categories

--- src/Components/Popup/helpNumberAndCategoryEditPopupContent.jsx
+++ src/Components/Popup/helpNumberAndCategoryEditPopupContent.jsx
@@ -3,25 +3,40 @@ import { disconnect } from '../../functions/disconnect'
 import { toast } from 'react-toastify'
 
 const HelpNumberAndCategoryEditPopupContent = ({ type, onClose }) => {
-  const [formData, setFormData] = useState({ name: '', phone: '' })
-  const [items, setItems] = useState([])
+  const [formData, setFormData] = useState({ name: '', telephone: '' })
+  const [numbers, setNumbers] = useState([])
+  const [categories, setCategories] = useState([])
   const [selectedItem, setSelectedItem] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState('')
 
   useEffect(() => {
-    const fetchItems = async () => {
+    const fetchCategories = async () => {
+      const categoryUrl = process.env.REACT_APP_BACKEND_URL + '/user/helpNumbersCategories'
+      const response = await fetch(categoryUrl, {
+        method: 'GET',
+        headers: {
+          'x-auth-token': sessionStorage.getItem('token'),
+          'Content-Type': 'application/json'
+        }
+      })
+      if (response.status === 401) {
+        disconnect()
+      }
+      if (response.ok) {
+        const data = await response.json()
+        setCategories(data)
+      } else {
+        toast.error('Erreur lors de la récupération des catégories.')
+      }
+    }
+
+    const fetchNumbers = async () => {
       setLoading(true)
       setError('')
 
-      const categoryUrl = process.env.REACT_APP_BACKEND_URL + '/user/helpNumbersCategories'
-      const numberUrl = process.env.REACT_APP_BACKEND_URL + '/user/helpNumbers'
-      const url = type === 'number' ? numberUrl : categoryUrl
-
-      console.log('Fetching data from:', url)
-
       try {
-        const response = await fetch(url, {
+        const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/user/helpNumbers', {
           headers: {
             'x-auth-token': sessionStorage.getItem('token'),
             'Content-Type': 'application/json'
@@ -38,7 +53,7 @@ const HelpNumberAndCategoryEditPopupContent = ({ type, onClose }) => {
         }
 
         const data = await response.json()
-        setItems(data)
+        setNumbers(data)
       } catch (error) {
         console.error('Error fetching data:', error)
         setError(error.message)
@@ -47,17 +62,21 @@ const HelpNumberAndCategoryEditPopupContent = ({ type, onClose }) => {
       }
     }
 
-    fetchItems()
+    fetchNumbers()
+    fetchCategories()
   }, [type])
 
   const handleSelectChange = (e) => {
     const selectedId = e.target.value
-    const item = items.find((item) => item._id === selectedId)
+    const item = numbers.find((item) => item._id === selectedId)
     setSelectedItem(item)
-    if (item) {
-      setFormData({ name: item.name, phone: item.phone || '' })
+    if (item && type === 'number') {
+      setFormData({ name: item.name, telephone: item.telephone || '' })
+    } else {
+      setFormData({ name: item.name})
     }
   }
+  console.log(type)
 
   const handleInputChange = (e) => {
     const { name, value } = e.target
@@ -74,7 +93,9 @@ const HelpNumberAndCategoryEditPopupContent = ({ type, onClose }) => {
     const categoryUpdateUrl = `${process.env.REACT_APP_BACKEND_URL}/adm/helpNumbersCategory/${selectedItem._id}`
     const url = type === 'number' ? numberUpdateUrl : categoryUpdateUrl
 
-    console.log('URL de requête:', url)
+    if (type === 'number') {
+delete formData.category    }
+
     console.log('Données envoyées:', formData)
 
     try {
@@ -93,7 +114,7 @@ const HelpNumberAndCategoryEditPopupContent = ({ type, onClose }) => {
         throw new Error(`Error ${response.status}: ${errorData.message || 'Unknown error'}`)
       }
 
-      toast.error('Modification réussie !')
+      toast.success('Modification réussie !')
       onClose()
     } catch (error) {
       console.error('Erreur lors de la requête:', error)
@@ -114,7 +135,12 @@ const HelpNumberAndCategoryEditPopupContent = ({ type, onClose }) => {
             Sélectionnez {type === 'number' ? 'le numéro d’aide' : 'la catégorie'} à modifier :
             <select onChange={handleSelectChange} value={selectedItem?._id || ''}>
               <option value=''>-- Sélectionnez --</option>
-              {items.map((item) => (
+              {type === 'number' && numbers.map((item) => (
+                <option key={item._id} value={item._id}>
+                  {item.name}
+                </option>
+              ))}
+              {type !== 'number' && categories.map((item) => (
                 <option key={item._id} value={item._id}>
                   {item.name}
                 </option>
@@ -123,25 +149,35 @@ const HelpNumberAndCategoryEditPopupContent = ({ type, onClose }) => {
           </label>
           {selectedItem && (
             <>
-              <label>
-                Nom:
-                <input
-                  type='text'
-                  name='name'
-                  value={formData.name}
-                  onChange={handleInputChange}
-                />
+              <label className='input-label'>
+                <span className='label-content'>Nom <span style={{ color: 'red' }}>*</span></span>
+                <input type='text' name='name' placeholder='Nom' value={formData.name} onChange={handleInputChange} />
               </label>
               {type === 'number' && (
-                <label>
-                  Numéro:
-                  <input
-                    type='text'
-                    name='phone'
-                    value={formData.phone}
-                    onChange={handleInputChange}
-                  />
-                </label>
+                <>
+                  <label className='input-label'>
+                    <span className='label-content'>Catégorie</span>
+                    <select data-testid='category-select' name="helpNumbersCategory" value={formData.helpNumbersCategory} onChange={handleInputChange}>
+                      {categories.map((option, index) => (
+                        <option key={index} value={option._id}>
+                          {option.name}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                  <label className='input-label'>
+                    <span className='label-content'>Numéro de Téléphone</span>
+                    <input type='text' name='telephone' placeholder='0000000000' value={formData.telephone} onChange={handleInputChange} />
+                  </label>
+                  <label className='input-label'>
+                    <span className='label-content'>Adresse Email</span>
+                    <input type='text' name='email' placeholder='prenom.nom.Schood1@schood.fr' value={formData.email}  onChange={handleInputChange} />
+                  </label>
+                  <label className='input-label'>
+                    <span className='label-content'>Description</span>
+                    <textarea name='description' placeholder="Une description à propos de l'aide fournie" value={formData.description} onChange={handleInputChange} />
+                  </label>
+                </>
               )}
               <button type='button' onClick={handleSubmit}>Sauvegarder</button>
             </>
