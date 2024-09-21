5c7ba8ae2e8f06032f3bc8be21ee8a13b12a31bd
adbusnel <adrien.busnel@epitech.eu>
2024-09-21 11:27:29 +0200

    [MOD] fixed css for chat, moved adding members and leaving conversation into same popup

--- src/Components/Popup/conversationCreation.jsx
+++ src/Components/Popup/conversationCreation.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../css/Components/Popup/popup.scss'
 import Select from 'react-select'
 
-const ConversationCreationPopupContent = ({ contacts, createConversation, closeCreateConversationPopup, isAddingParticipants }) => {
+const ConversationCreationPopupContent = ({ contacts, createConversation, closeCreateConversationPopup, isAddingParticipants, members = null }) => {
   const [selectedContacts, setSelectedContacts] = useState([])
   const [convTitle, setConvTitle] = useState('')
+  const [filteredContacts, setFilteredContacts] = useState(contacts)
 
+  useEffect(() => {
+    if (members) {
+      const c = contacts.filter(contact =>
+        !members.some(member => member._id === contact._id)
+      );
+      setFilteredContacts(c)
+    }
+  }, [])
   const handleCreateConversation = () => {
     const ids = []
-    selectedContacts.forEach((contact) => (
-      ids.push(contact._id)
+    selectedContacts.forEach((filteredContacts) => (
+      ids.push(filteredContacts._id)
     ))
     if (ids.length === 0) {
       return
@@ -34,13 +43,13 @@ const ConversationCreationPopupContent = ({ contacts, createConversation, closeC
   return (
     <>
       <label className='input-label'>
-        <span className='label-content'>Rechercher un ou plusieurs membre(s) <span style={{ color: 'red' }}>*</span></span>
+        {isAddingParticipants ? <span className='label-content'>Sélectionner un/des utilisateur(s)</span> : <span className='label-content'>Rechercher un ou plusieurs membre(s) <span style={{ color: 'red' }}>*</span></span>}
         <Select
           isMulti
           data-testid='select-contacts'
           id='select-contacts'
           placeholder='Rechercher un contact'
-          options={contacts}
+          options={filteredContacts}
           value={selectedContacts}
           onChange={handleSelectChange}
           getOptionValue={(option) => option._id}
@@ -56,6 +65,14 @@ const ConversationCreationPopupContent = ({ contacts, createConversation, closeC
       <button className='popup-btn' onClick={handleCreateConversation}>
         {isAddingParticipants ? 'Ajouter le membre' : 'Créer la conversation'}
       </button>
+      {isAddingParticipants && (
+        <div>
+          <h3>Membres actuels ({members.length})</h3>
+          {members && members.map((member,  index) => (
+            <p key={index}>{member.firstname} {member.lastname}</p>
+          ))}
+        </div>
+      )}
     </>
   )
 }
