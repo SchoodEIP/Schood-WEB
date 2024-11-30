98aac6f6f139d559c2c72b9da3d8c1ba57b62318
adbusnel <adrien.busnel@epitech.eu>
2024-11-30 18:22:04 +0100

    [ADD] added basic desannonym demands

---
+++ src/Components/Popup/desanonymFeeling.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+import '../../css/Components/Popup/popup.scss'
+import '../../css/pages/createAlerts.scss'
+
+const DesanonymFeelingPopupContent = ({ handleAskDesanonym}) => {
+  const [message, setMessage] = useState('')
+
+  const handleMessage = (event) => {
+    console.log(event)
+    setMessage(event.target.value)
+  }
+
+  return (
+    <>
+      <input placeholder="Message pour l'Élève..." onChange={handleMessage} type="text" value={message}/>
+
+      <button className='popup-btn' onClick={() => handleAskDesanonym(message)}>Faire la demande</button>
+    </>
+  )
+}
+
+export default DesanonymFeelingPopupContent
