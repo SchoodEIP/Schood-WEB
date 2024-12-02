ab863f4ee07f059ba99e0ce09cc2cd7b0c5f08e7
adbusnel <adrien.busnel@epitech.eu>
2024-12-02 15:06:13 +0100

    [MOD] shows banned accounts

--- src/Components/Popup/deleteAccount.jsx
+++ src/Components/Popup/deleteAccount.jsx
@@ -3,6 +3,16 @@ import '../../css/Components/Popup/popup.scss'
 import '../../css/pages/createAlerts.scss'
 
 const DeleteAccountPopupContent = ({ userIdValue, actionType, deleteUserAccount, activateAccount, closeDeleteAccountPopup }) => {
+
+  const handleAction = (action) => {
+    if (!action) {
+      deleteUserAccount(false, userIdValue)
+    } else {
+      activateAccount(userIdValue)
+    }
+    closeDeleteAccountPopup()
+  }
+
   return (
     <>
       <div>
@@ -14,8 +24,8 @@ const DeleteAccountPopupContent = ({ userIdValue, actionType, deleteUserAccount,
         actionType === 'delete'
           ? <button className='popup-btn' style={{ backgroundColor: 'red', borderColor: 'red' }} onClick={() => deleteUserAccount(true, userIdValue)}>Supprimer</button>
           : actionType === 'suspend'
-            ? <button className='popup-text-btn' onClick={() => deleteUserAccount(false, userIdValue)}>Suspendre</button>
-            : <button className='popup-text-btn' onClick={() => activateAccount(userIdValue)}>Restaurer</button>
+            ? <button className='popup-text-btn' onClick={() => handleAction(false)}>Suspendre</button>
+            : <button className='popup-text-btn' onClick={() => handleAction(true)}>Restaurer</button>
       }
       <button className='popup-btn' onClick={closeDeleteAccountPopup}>Annuler</button>
     </>
